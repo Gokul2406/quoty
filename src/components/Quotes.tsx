09996be6be4fc902ddc7b1e5dc89cbb59732258f
Gokul2406 <gokulpbharathan89@gmail.com>
2021-04-01 13:04:30 +0530

    Added quote recieving functionality

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-
+type Quote = {
+  content: string,
+  author: string
+}
 
 class Quotes extends Component {
   constructor(props: any) {
   super(props);
   this.state = {
-    loading: false
+    loading: false,
+    quoteContent: "",
+    quoteAuthor: ""
   }
 
   }
@@ -14,15 +19,18 @@ class Quotes extends Component {
   async componentDidMount() {
     const url: string = "https://api.quotable.io/random"
     const response = await fetch(url)
-    const data = await response.json();
-    console.log(data);
+    const data: Quote = await response.json();
+    this.setState(
+  {quoteContent: data.content, quoteAuthor: data.author})
+  console.log(this.state)
   }
 
   render() {
 
   return(
+    <div className="quotes">
     <h1>Hello from Quotes</h1>
-  
+      </div>
   )
 
   }
