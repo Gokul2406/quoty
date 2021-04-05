3919b7e4ecbae3141fbef59fdb8a32ea5a3fd098
Gokul2406 <gokulpbharathan89@gmail.com>
2021-04-05 13:57:35 +0530

    Added material ui appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Quotes from './components/Quotes';
 
 const App: React.FC = () => {
 
   return(
     <div className="app">
+    <Navbar />
     <Quotes />
+
     </div>
   )
 
